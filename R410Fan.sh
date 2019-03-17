4cb08f6792c58eb0c15ffd0fc27203071560adf6
Kevin Simmons <kellosmith@gmail.com>
2019-03-17 02:51:57 -0500

    Add files via upload

---
+++ R410Fan.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------------------------------
+# Script for checking the temperature reported by the ambient temperature sensor, 
+# and if deemed to high send the raw IPMI command to enable dynamic fan control.
+#
+# Requires ipmitool – apt-get install ipmitool
+# Tested on Ubuntu 17.04
+# ----------------------------------------------------------------------------------
+
+
+# IPMI DEFAULT R410 SETTINGS
+# Modify to suit your needs.
+IPMIHOST=192.168.10.16
+IPMIUSER=root
+IPMIPW=Wireless-12
+
+# TEMPERATURE
+# Change this to the temperature in celcius you are comfortable with. 
+# If it goes above it will send raw IPMI command to enable dynamic fan control
+MAXTEMP=28
+
+# This variable sends a IPMI command to get the temperature, and outputs it as two digits.
+# Do not edit unless you know what you do.
+TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW sdr type temperature |grep Ambient |grep degrees |grep -Po '\d{2}' | tail -1)
+
+
+if [[ $TEMP > $MAXTEMP ]];
+  then
+    printf "Temperature is too high! ($TEMP C) Activating dynamic fan control!" | systemd-cat -t 4710-IPMI-TEMP
+    echo "Temperature is too high! ($TEMP C) Activating dynamic fan control!"
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x01
+  else
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x00
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x02 0xff 0x23
+    printf "Temperature is OK ($TEMP C)" | systemd-cat -t R410-IPMI-TEMP
+    echo "Temperature is OK ($TEMP C)"
+fi
---
+++ R510Fan.sh
@@ -0,0 +1,55 @@
+# ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW  sdr type fan
+ 
+# Enable Manual fan speed
+# ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x00
+ 
+# Set fan speed to auto
+# ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x01
+ 
+# IPMI SETTINGS:
+IPMIHOST=192.168.10.15
+IPMIUSER=root
+IPMIPW=Wireless-12
+ 
+# TEMPERATURE
+# If the temperature goes above the set degrees it will send raw IPMI command to enable dynamic fan control
+MAXTEMP=32
+WARNTEMP=29
+ 
+# This variable sends a IPMI command to get the temperature, and outputs it as two digits.
+TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW sdr get "Ambient Temp" | grep "Sensor Reading" | awk '{print $4}')
+ 
+ 
+# R510 Fan Speeds
+# 0x11 3120rpm
+# 0x10 3000rpm
+# 0x09 2280rpm
+# 0x08 2160rpm
+# 0x07 2040rpm
+# 0x06 1920rpm
+# 0x05 1800rpm
+# 0x04 1680rpm
+ 
+ 
+ 
+if [[ $TEMP > $MAXTEMP ]];
+  then
+    #echo "Temperature is BAD ($TEMP C)"
+    printf "R510 Temperature is BAD ($TEMP C)" | systemd-cat -t R510-IPMI-TEMP
+    # Set fan speed to auto
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x01
+ 
+elif [[ $TEMP > $WARNTEMP ]];
+  then
+    #echo "Temperature is WARN ($TEMP c)"
+    printf "R510 Temperature is WARN ($TEMP C)" | systemd-cat -t R510-IPMI-TEMP
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x00
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x02 0xff 0x1B
+ 
+else
+    #echo "Temperature is OK ($TEMP C)"
+    printf "R510 Temperature is OK ($TEMP C)" | systemd-cat -t R510-IPMI-TEMP
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x00
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x02 0xff 0x17
+fi
+ 
